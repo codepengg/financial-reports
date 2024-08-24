8b46def14f0c74c7e4d7e50a5cbfdcc28a1d6287
pengg <codepengg@gmail.com>
2024-08-24 12:47:18 +0700

    feat: debug mailtrap

--- app/Filament/Pages/Auth/Register.php
+++ app/Filament/Pages/Auth/Register.php
@@ -9,28 +9,30 @@ use Filament\Facades\Filament;
 use Filament\Http\Responses\Auth\Contracts\RegistrationResponse;
 use Filament\Notifications\Notification;
 use Filament\Pages\Auth\Register as BaseRegister;
+
 class Register extends BaseRegister
 {
     protected $register;
+
     public function register(): ?RegistrationResponse
     {
-        try {
-            $this->rateLimit(2);
-        } catch (TooManyRequestsException $exception) {
-            Notification::make()
-                ->title(__('filament-panels::pages/auth/register.notifications.throttled.title', [
-                    'seconds' => $exception->secondsUntilAvailable,
-                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
-                ]))
-                ->body(array_key_exists('body', __('filament-panels::pages/auth/register.notifications.throttled') ?: []) ? __('filament-panels::pages/auth/register.notifications.throttled.body', [
-                    'seconds' => $exception->secondsUntilAvailable,
-                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
-                ]) : null)
-                ->danger()
-                ->send();
-
-            return null;
-        }
+//        try {
+//            $this->rateLimit(2);
+//        } catch (TooManyRequestsException $exception) {
+//            Notification::make()
+//                ->title(__('filament-panels::pages/auth/register.notifications.throttled.title', [
+//                    'seconds' => $exception->secondsUntilAvailable,
+//                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
+//                ]))
+//                ->body(array_key_exists('body', __('filament-panels::pages/auth/register.notifications.throttled') ?: []) ? __('filament-panels::pages/auth/register.notifications.throttled.body', [
+//                    'seconds' => $exception->secondsUntilAvailable,
+//                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
+//                ]) : null)
+//                ->danger()
+//                ->send();
+//
+//            return null;
+//        }
 
         $data = $this->form->getState();
 
